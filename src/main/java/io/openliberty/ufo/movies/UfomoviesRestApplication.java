4ac3a3835fa90659ac44280a8d9770858d790862
Andy McCright <j.andrew.mccright@gmail.com>
2020-06-15 14:03:11 -0500

    Initial commit from start.microprofile.io
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>

---
+++ src/main/java/io/openliberty/ufo/movies/UfomoviesRestApplication.java
@@ -0,0 +1,11 @@
+package io.openliberty.ufo.movies;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class UfomoviesRestApplication extends Application {
+}
